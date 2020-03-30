7c151219be8969819c6639076814852917b30de2
Jay <39040834+jayn0354@users.noreply.github.com>
2020-03-29 21:39:17 -0700

    edit button
    starting edit button creation, just a outline using material-ui
    
    could use reactstrap

--- client/src/components/VerifiedStudents/VerifiedStudents.js
+++ client/src/components/VerifiedStudents/VerifiedStudents.js
@@ -14,6 +14,12 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import EditIcon from '@material-ui/icons/Edit';
 import TablePagination from '@material-ui/core/TablePagination';
+import Modal from '@material-ui/core/Modal';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 import axios from 'axios';
 
 
@@ -26,12 +32,15 @@ class VerifiedStudents extends React.Component {
             students: [],
             search: '',
             page: 0,
+            isOpen: false,
+            value: false, // placeholder for the modal
         }
 
         this.handleChangePage = this.handleChangePage.bind(this);
+
     }
 
-    componentDidMount(){
+    componentDidMount() {
         console.log("component mounted in the VerifiedStudents page");
         var verifiedAccounts;
         axios.get('http://localhost:5000/verified-account-information')
@@ -53,7 +62,23 @@ class VerifiedStudents extends React.Component {
 
     handleChangePage = (event, newPage) => {
         this.setState({ page: newPage })
-    };
+    }
+
+    editButtonTest() {
+        console.log("Yeetus");
+    }
+
+    handleOpen = () => {
+        this.setState(
+            { isOpen: true }
+        )
+    }
+
+    handleClose = () => {
+        this.setState(
+            { isOpen: false }
+        )
+    }
 
     renderDash() {
         let filteredStudents = this.state.students.filter(
@@ -63,8 +88,8 @@ class VerifiedStudents extends React.Component {
             }
         );
 
-        
-        return(
+
+        return (
             <div className="backgroundDash">
                 <Menu />
                 <Sidebar />
@@ -91,19 +116,43 @@ class VerifiedStudents extends React.Component {
                                         <TableCell align="right">{student.shortId}</TableCell>
                                         <TableCell align="right">{student.longId}</TableCell>
                                         <TableCell align="right">{student.grade}</TableCell>
-                                        <TableCell align="right"><Button style={{ position: 'relative', left: '15px' }}><EditIcon color="primary" /></Button></TableCell>
+                                        <TableCell align="right"><Button style={{ position: 'relative', left: '15px' }} onClick={this.editButtonTest, this.handleOpen}><EditIcon color="primary" /></Button></TableCell>
+                                        <Modal open={this.state.isOpen} onClose={this.handleClose}>
+                                            <div className="modal">
+                                                <h2>Edit Student Name</h2>
+                                                <FormControl component="fieldset">
+                                                    <FormLabel component="legend">ASB</FormLabel>
+                                                    <RadioGroup name="ASB" value={this.state.value} onChange={this.handleChangeRadio}>
+                                                        <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                                                        <FormControlLabel value="no" control={<Radio />} label="No" />
+                                                    </RadioGroup>
+                                                    <FormLabel component="legend">Off Sixth Period</FormLabel>
+                                                    <RadioGroup name="offsix" value={this.state.value} onChange={this.handleChangeRadio}>
+                                                        <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                                                        <FormControlLabel value="no" control={<Radio />} label="No" />
+                                                    </RadioGroup>
+                                                    <FormLabel component="legend">Off Lunch</FormLabel>
+                                                    <RadioGroup name="offlunch" value={this.state.value} onChange={this.handleChangeRadio}>
+                                                        <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                                                        <FormControlLabel value="no" control={<Radio />} label="No" />
+                                                    </RadioGroup>
+                                                </FormControl>
+                                            </div>
+                                        </Modal>
                                     </TableRow>
+
                                 ))}
                             </TableBody>
                         </Table>
-                        </TableContainer>
-                        <TablePagination
-                        style={{position: 'relative', right: "810px"}}
-                        count={filteredStudents.length} 
+                    </TableContainer>
+                    <TablePagination
+                        style={{ position: 'relative', right: "810px" }}
+                        count={filteredStudents.length}
                         rowsPerPage={15}
                         page={this.state.page}
                         component="div"
                         onChangePage={this.handleChangePage} />
+
                 </Card>
             </div>
         )
